f32a5ae8f6a777fea95b24048a13bba67daf356a
Matthew Spencer <spencer.ma@protonmail.com>
2020-08-31 23:24:55 +0100

    Added git configuration and git secrets.

--- install-windows.cmd
+++ install-windows.cmd
@@ -1,12 +1,18 @@
 @echo off
 set DOTFILES=%cd%
 
-rem vscode
+:: vscode
 copy /y %DOTFILES%\vscode\ %APPDATA%\Code\User
 call code --install-extension ms-dotnettools.csharp
 call code --install-extension msjsdiag.debugger-for-chrome
 call code --install-extension sdras.night-owl
 call code --install-extension shardulm94.trailing-spaces
 
-rem cmder
+:: cmder
 copy /y %DOTFILES%\cmder\ConEmu.xml %ConEmuDir%
+
+:: git
+git config --global core.excludesfile %DOTFILES%\git\.gitignore
+
+:: secrets
+.\secrets-windows
