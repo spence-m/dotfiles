dd972d5311f4500a96485339a7718e395504343a
Matthew Spencer <spencer.ma@protonmail.com>
2021-05-22 10:24:20 +0100

    Removed cmder as I do not use it anymore.

--- install-windows.cmd
+++ install-windows.cmd
@@ -8,11 +8,6 @@ call code --install-extension msjsdiag.debugger-for-chrome
 call code --install-extension sdras.night-owl
 call code --install-extension shardulm94.trailing-spaces
 
-:: cmder
-copy /y %DOTFILES%\cmder\ConEmu.xml %ConEmuDir%
-copy /y %DOTFILES%\cmder\user_profile.cmd %CMDER_ROOT%\config
-copy /y %DOTFILES%\cmder\user_profile.sh %CMDER_ROOT%\config
-
 :: git
 git config --global include.path %DOTFILES%\git\.gitconfig
 git config --global core.excludesfile %DOTFILES%\git\.gitignore
